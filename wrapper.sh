8cc92d74e0650722053718e218439988a65e4be4
Lukewh <luke@lukewh.com>
2022-05-06 14:12:26 +0200

    Add wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CMD="${1}"
+
+if [ "${CMD}" == "upgrade" ]; then
+    echo "You are running the Deno snap. The upgrade command is unavailable."
+    echo "The snap is updated automatically after a new release of Deno."
+    echo "You can force an update by running the command:"
+    echo "    snap refresh deno"
+    echo ""
+    echo "If you think there is an issue, create an issue at https://github.com/Lukewh/deno-snap"
+    exit 1
+fi
+
+$SNAP/bin/deno "${@}"
